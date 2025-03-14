4b71f411633299de2292b8766bbc20bafcfffd06
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-03-14 23:58:39 +0500

    Components created and some code of line add to check run properly  03/14/2025

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Article, CTA, Brand, Feature, Navbar } from './components/AllComponents'
 
 const App = () => {
-  return (
-    <div>
-        <h1>Let's Check it run properly our Application</h1>
-    </div>
-  )
+    return (
+        <>
+            <div>
+                <h1>Let's Check it run properly our Application</h1>
+
+            </div>
+            <Article/>
+            <Brand/>
+            <CTA/>
+            <Feature/>
+            <Navbar/>
+        </>
+    )
 }
 
 export default App
