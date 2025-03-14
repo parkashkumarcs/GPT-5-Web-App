8f0e4c822ff015f50e88a1f9efeff16511eb2a6c
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-03-14 22:48:08 +0500

    Deleted Old src file and created new file with index.js codes  03/14/2025

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDOM from 'react-dom'; 
+
+//Following code for importing App  file
+import App from './App'
+reactDOM.render(<App />, document.getElementById('root'));
