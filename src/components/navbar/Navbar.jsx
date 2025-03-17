efa170e5247322e5a17784650e6214c31415f14e
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-03-17 23:01:59 +0500

    navbar.jsx and css file codes added and logo designed for gpt-5 03/17/2025

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
+import gptLogo from '../../assets/ChatGPT-Logo.png';
 
 
 const Navbar = () => {
   return (
     <div className='gpt5__navbar'>
       <div className='gpt5__navbar_links'>
-
+          <div className='gpt5_logo-container'>
+            <img src={gptLogo}></img>
+            <span>GPT-5</span>
+          </div>
       </div>
     </div>
   )
