804e7dd173be2cfd014738377d5d651df5fb16ee
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-03-24 19:20:48 +0500

    cta components design created and updated 03/24/2025

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ const App = () => {
             <Features />
             
             <Possibility />
-            {/* 
+             
             <CTA />
+            {/*
             <Blog />
             <Footer /> */}
 
--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='gpt5__cta'>
+      <div className='gpt5__cta-content'>
+        <p>Request Early Access To Get Started</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+        <div className='gpt5__cta-btn'>
+          <button type='button'>Get Started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
