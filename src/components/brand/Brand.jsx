4598065f86f741a79900b7767572d52cb616ce00
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-04-12 15:57:21 +0500

    All links added to brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -21,40 +21,40 @@ const Brand = () => {
   return (
     <div className='brand__Section-container sec__padding'>
       <div className='brand__Image-container'>
-        <img src= {amazon}></img>
+          <a href="https://www.amazon.com"><img src= {amazon}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= { playstore}></img>
+        <a href="https://play.google.com"><img src= {playstore}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {apple}></img>
+        <a href="https://www.apple.com"><img src= {apple}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {shopify}></img>
+        <a href="https://www.shopify.com"><img src= {shopify}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {slack}></img>
+        <a href="https://www.slack.com"><img src= {slack}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {vivo}></img>
+        <a href="https://www.vivo.com"><img src= {vivo}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {youtube}></img>
+      <a href="https://www.youtube.com"><img src= {youtube}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {discord}></img>
+        <a href="https://www.discord.com"><img src= {discord}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {facebook}></img>
+        <a href="https://www.facebook.com"><img src= {facebook}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {deepseek}></img>
+        <a href="https://www.deepseek.com"><img src= {deepseek}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {atlassian}></img>
+        <a href="https://www.atlassian.com"><img src= {atlassian}></img></a>
       </div>
       <div className='brand__Image-container'>
-        <img src= {gemini}></img>
+        <a href="https://gemini.google.com"><img src= {gemini}></img></a>
       </div>
     </div>
   )
